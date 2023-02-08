87ee0415a3cde47d9ea9c72e45be32e8fbb5fe06
Admin_max <max20088888@mail.ru>
2023-02-08 14:46:59 +1000

    html_pars

--- selenium1.py
+++ selenium1.py
@@ -1,22 +1,15 @@
+
+
 import time
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
-urls = [
-    'https://www.faceit.com/ru/shop/csgo',
-    'https://www.figma.com/login',
-    'https://lordserials.org/zarubezhnye/7541-horoshij-doktor-2017.html',
-    'https://youtube.com/',
-    'https://www.twitch.tv/',
-    'https://gdz.ru',
-    'https://vk.com/im?peers=648937642&amp;sel=690918218',
-    'https://vk.com/b_sharnin',
-    'https://fonts.google.com/',
-    'https://www.freecodecamp.org/learn'
-]
+driver.get("http://www.python.org")
+page = driver.page_source
+file_ = open('page.html', 'w', encoding='utf-8')
+file_.write(page)
+file_.close()
 
-for i in urls:
-    driver.get(i)
 
