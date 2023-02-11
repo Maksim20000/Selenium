4588be54d5ab256109fc018a75ec6728a368913c
Admin_max <max20088888@mail.ru>
2023-02-11 17:18:14 +1000

    my_first_commit_beautiful_soup

---
+++ beautifulSoup/beautifulSoup.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+
+html_doc = """
+<html><head><title>История моей жизниy</title></head>
+<body>
+<p class="title"><b>История моей жизни</b></p>
+
+<p class="story">У меня есть три сестры:
+<a href="http://example.com/vera" class="sister" id="link1">Вера</a>,
+<a href="http://example.com/nadjda" class="sister" id="link2">Надежда</a> and
+<a href="http://example.com/lubov" class="sister" id="link3">Любовь</a>;
+И все у них хорошо</p>
+
+<p class="story">...</p>
+"""
+
+soup = BeautifulSoup(html_doc, 'html.parser')
+print(soup) #pretty красриво
