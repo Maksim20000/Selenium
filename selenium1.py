e7f40b9fef095f36d0c7c3aba277dc3d8bed9ab4
Admin_max <max20088888@mail.ru>
2023-02-11 17:10:27 +1000

    L_am_maybe

--- selenium1.py
+++ selenium1.py
@@ -1,15 +1,26 @@
-
-
 import time
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
-driver.get("http://www.python.org")
-page = driver.page_source
-file_ = open('page.html', 'w', encoding='utf-8')
-file_.write(page)
-file_.close()
+urls = [
+    'https://www.faceit.com/ru/shop/csgo',
+    'https://www.figma.com/login',
+]
+
+num = 0
+
+for i in urls:
+    name = f'Site_Nomer{num}'
+    # print(f'Привт,{i}')
+    driver.get(i)
+    page = driver.page_source
+    file_ = open(f"page_{num}.html", 'w', encoding='utf-8')
+    file_.write(page)
+    file_.close()
+    num += 1
+
+
 
 
