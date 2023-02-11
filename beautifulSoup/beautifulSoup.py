a11761845d3a0ac42d48f44352df770ba567aa0b
Admin_max <max20088888@mail.ru>
2023-02-11 18:13:13 +1000

    src_commit

--- beautifulSoup/beautifulSoup.py
+++ beautifulSoup/beautifulSoup.py
@@ -1,44 +1,23 @@
+import time
+from selenium import webdriver
 from bs4 import BeautifulSoup
+from webdriver_manager.chrome import ChromeDriverManager
 
-html_doc = """
-<html>
+driver = webdriver.Chrome(ChromeDriverManager().install())
 
-<head>
-    <title>
-        example
-    </title>
-</head>
+#'html.parser' виды паксеров
+#features="lxml"
 
-<body>
-    <ul>
-        <li class = 'pip'>Главная страница</li>
-        <li>Товары</li>
-        <li>Контакты</li>
-        <li>Корзина</li>
-    </ul>
-    <div>
-        <p>Информация о чем то тут..</p>
-        <p>И тут..</p>
-    </div>
-    <div class='main'>
-        <input type="text" placeholder="Напиши" class="input-form">
-    </div>
-</body>
+driver.get("http://www.python.org")
+soup = BeautifulSoup(driver.page_source, features="lxml")
 
-</html>
-"""
+print(soup.prettify())
 
-soup = BeautifulSoup(html_doc, 'html.parser')
-# firstSoupeLet = soup.findAll('div')
-# secondSoupe = soup.find('input', _class_="input-form")
+for link in soup.find_all('a'):
+    url = link.get('href') #Берет все ссылки из ссылок
+    if 'https' in url or 'http' in url:
+        driver.get(url)
 
-
-# a = soup.findAll('li')
-# for i in a:
-#     print(i.text)
-
-# i = soup.find('li', class_='pip').text
-# print(i)
-
-# i = soup.find('li', class_='pip')
-# print(i.text)
+    else:
+        driver.get('https://www.python.org' + url)
+    time.sleep(3)
