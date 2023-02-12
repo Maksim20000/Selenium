0ef2fbae8092ced6dac9aa9699d71449f659bac1
Admin_max <max20088888@mail.ru>
2023-02-12 19:50:12 +1000

    my_home_work_Beautiful_soup_and_all_lesson_code

--- beautifulSoup/beautifulSoup.py
+++ beautifulSoup/beautifulSoup.py
@@ -8,12 +8,55 @@ driver = webdriver.Chrome(ChromeDriverManager().install())
 #'html.parser' виды паксеров
 #features="lxml"
 
-driver.get("https://ru.wikipedia.org/wiki/%D0%A1%D0%BF%D0%B8%D1%81%D0%BE%D0%BA_%D0%B3%D0%BE%D1%81%D1%83%D0%B4%D0%B0%D1%80%D1%81%D1%82%D0%B2")
-soup = BeautifulSoup(driver.page_source, features="lxml")
 
-table = soup.find('table', class_='wikitable').find('tbody')
+urls = [
+    'https://www.faceit.com/ru/shop/csgo',
+    'https://www.figma.com/login',
+]
 
-counties = []
+num = 0
 
-for i in table.findAll('td'):
-    print(i.text)
+for i in urls:
+    name = f'Site_Nomer{num}'
+    # print(f'Привт,{i}')
+    driver.get(i)
+    page = driver.page_source
+    file_ = open(f"page_{num}.html", 'w', encoding='utf-8')
+    file_.write(page)
+    file_.close()
+    num += 1
+
+
+
+
+driver.get('https://www.kodland.org/')
+soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+allButtons = soup.findAll('input')
+
+openHtmlFile = open('kd.html', 'w', encoding='UTF-8')
+openHtmlFile.write(str(allButtons))
+openHtmlFile.close()
+
+
+
+soup = BeautifulSoup(html_doc, 'html.parser')
+print(soup) #pretty красриво
+
+
+soup = BeautifulSoup(html_doc, 'html.parser')
+firstSoupeLet = soup.find('div', class_='main')
+print(firstSoupeLet.prettify())
+# firstSoupeLet = soup.findAll('div')
+# secondSoupe = soup.find('input', _class_="input-form")
+
+
+# a = soup.findAll('li')
+# for i in a:
+#     print(i.text)
+
+# i = soup.find('li', class_='pip').text
+# print(i)
+
+# i = soup.find('li', class_='pip')
+# print(i.text)
