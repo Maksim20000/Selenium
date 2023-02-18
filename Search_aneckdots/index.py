cce820bc921f0c2edfea6a7a0a637bf2a0749b7c
Admin_max <max20088888@mail.ru>
2023-02-18 19:50:40 +1000

    search_anecdots_conspect_is_finish_commit

--- Search_aneckdots/index.py
+++ Search_aneckdots/index.py
@@ -6,15 +6,37 @@
 
 # for i in range(11):
 #     print(f'https://platform.kodland.org/ru/change_task_{i}/')
+# for i in range(1, 5, 1):
+#     driver.get(f'https://nekdo.ru/page/{i}/')
+#     time.sleep(5)
+#
+# import time
+# from selenium import webdriver
+# from bs4 import BeautifulSoup
+# from webdriver_manager.chrome import ChromeDriverManager
+# import pickle
+#
+# driver = webdriver.Chrome(ChromeDriverManager().install())
+#
+# jockes = []
+#
+# for i in range(1, 3):
+#     url = f'https://nekdo.ru/page/{i}/'
+#     driver.get(url)
+#     soup = BeautifulSoup(driver.page_source, features="lxml")
+#     divs = soup.findAll('div', class_='text')
+#     for div in divs:
+#         jock = div.text + '\n'
+#         jockes.append(jock)
+#     time.sleep(2)
+#
+# with open('anecs.pickle', 'wb') as f:
+#     pickle.dump(jockes, f)
+#
 
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-import time
+#Код чтения файла  pickle:
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
+import pickle
 
-for i in range(1, 5, 1):
-    driver.get(f'https://nekdo.ru/page/{i}/')
-    time.sleep(5)
-
-
+favcolor = pickle.load(open("anecs.pickle", "rb"))
+print(favcolor)
