8c66fc172a80893f5a751ea1f90b288bf20cbbab
Admin_max <max20088888@mail.ru>
2023-02-23 21:39:48 +1000

    l_did_my_home_hork

--- Search_aneckdots/home.py
+++ Search_aneckdots/home.py
@@ -1,23 +1,35 @@
-import time
-from selenium import webdriver
-from bs4 import BeautifulSoup
-from webdriver_manager.chrome import ChromeDriverManager
-import pickle
-
-driver = webdriver.Chrome(ChromeDriverManager().install())
-
-soup = BeautifulSoup(driver.page_source, features="lxml")
-url = 'https://www.python.org/downloads/'
-driver.get(url)
-
-divs = soup.find_all('span', class_='release-number')
-print(divs)
+# import time
+# from selenium import webdriver
+# from bs4 import BeautifulSoup
+# from webdriver_manager.chrome import ChromeDriverManager
+# import pickle
+#
+# driver = webdriver.Chrome(ChromeDriverManager().install())
+#
+# soup = BeautifulSoup(driver.page_source, features="lxml")
+# url = 'https://www.python.org/downloads/'
+# driver.get(url)
+#
+# divs = soup.find_all('span', class_='release-version')
+# print(divs)
 
 # pythons = []
 # for div in divs:
 #     pythons.append(div.text)
 #
 # print(pythons)
+
+
+
+from bs4 import BeautifulSoup
+import requests
+
+url = 'https://www.python.org/downloads/'
+response = requests.get(url)
+soup = BeautifulSoup(response.content, features="lxml")
+divs = soup.find_all('span', class_='release-version')
+for div in divs:
+    print(div.text)
 
 
 
