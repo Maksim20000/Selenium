4c7537dc728e6296da6eb9f911f2e92c339867e2
Admin_max <max20088888@mail.ru>
2023-03-04 18:37:05 +1000

    upgread_conspect-pytest-main.py-

--- testSelenium/main.py
+++ testSelenium/main.py
@@ -1,8 +1,53 @@
 # pip install pytest
 
+# first-algoritm
+#def inc(x):
+#   return x + 1
+#
+# def test_answer():
+#     assert inc(3) == 5
 
-def inc(x):
-    return x + 1
 
-def test_answer():
-    assert inc(3) == 5
+
+# second-algoritm
+# последовательность фебоначи
+
+# def multiply(x, y):
+#     return x * y
+#
+#
+# def testMultiply():
+#     assert multiply(3, 5) == 15
+#     assert multiply(5, 5) == 25
+#
+#
+#
+# def sum_vals(x, y):
+#     return x + y
+#
+#
+# def add_hello(string):
+#     return string + 'hello'
+#
+#
+# cache = {0: 0, 1: 1}
+#
+#
+# def fibonacci_of(n):
+#     if n in cache:
+#         return cache[n]
+#     cache[n] = fibonacci_of(n - 1) + fibonacci_of(n - 2)
+#     return cache[n]
+#
+#
+# # TESTS ==========================================================================
+# def test_fibonacci_of():
+#     # 0	1	1	2	3	5	8	13	21	34	55	89	144 - Первые несколько чисел Фибоначчи
+#     assert fibonacci_of(0) == 0
+#     assert fibonacci_of(1) == 1
+#     assert fibonacci_of(2) == 1
+#     assert fibonacci_of(3) == 2
+#     assert fibonacci_of(4) == 3
+#     assert fibonacci_of(5) == 5
+#     assert fibonacci_of(6) == 8
+
