1df77596c7b6b7f9cf9114bceb229826c9ac28df
Максим_Intern <110373927+Maksim20000@users.noreply.github.com>
2023-04-08 16:29:02 +1000

    update progect

--- finile progect/main.py
+++ finile progect/main.py
@@ -1,10 +1,7 @@
 import time
-from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
 from reduserce.filmRedusors import filmRedusers
 from reduserce.helpRedusor import helpRedusor
+from reduserce.dlViaioRedusor import dlViaioRedusor
 
 while True:
     command = input('Введите команду которую вы хотите ввести (Команда /help предоставляет все команды): ')
@@ -22,49 +19,21 @@ while True:
         time.sleep(1)
         print('А если у вас уже есть ссылка то нажмите на 1')
 #       Выбор знает ли человк как качать видио
-        searchYesOrNo = int(input('Введите, пожалуйста число: '))
+        searchYesOrNo = int(input('Введите число: '))
         if searchYesOrNo == 0:
             print('Вам нужно зайти на видио, которое вы хотите скачать')
-            print('Под этим видио будет кнопка поделиться')
-            print('Высвечивается ссылка, которую нужно вставить')
-    #       Функция с открытием бота
+            time.sleep(1)
+            print('Под этим видио будет кнопка поделиться, нажмите на нее')
+            time.sleep(1)
+            print('Высвечивается ссылка на видио')
+            time.sleep(1)
+
+            hrefVidio = input('Введите эту ссылку на видио: ')
+            dlViaioRedusor(hrefVidio)
 
         elif searchYesOrNo == 1:
             hrefVidio = input('Введите ссылку на видио: ')
-
-            driver = webdriver.Chrome(ChromeDriverManager().install())
-            driver.get('https://ru.savefrom.net/1-%D0%B1%D1%8B%D1%81%D1%82%D1%80%D1%8B%D0%B9-%D1%81%D0%BF%D0%BE%D1%81%D0%BE%D0%B1-%D1%81%D0%BA%D0%B0%D1%87%D0%B0%D1%82%D1%8C-%D1%81-youtube-160/')
-            # soup = BeautifulSoup(driver.page_source, features='lxml')
-
-            # Кнопка, которая ищет по ссылке видио
-            searchVidio = driver.find_element(By.XPATH, '//*[@id="sf_url"]')
-            searchVidio.send_keys(hrefVidio)
-
-            buttonSearchVidio = driver.find_element(By.XPATH, '//*[@id="sf_submit"]')
-            buttonSearchVidio.click()
-            time.sleep(5)
-
-            downloadVidio = driver.find_element(By.XPATH, '//*[@id="sf_result"]/div/div[1]/div[2]/div[2]/div[1]/a')
-            downloadVidio.click()
-
-            time.sleep(1)
-            print('20%')
-            time.sleep(1)
-            print('30%')
-            time.sleep(1)
-            print('40%')
-            time.sleep(1)
-            print('50%')
-            time.sleep(1)
-            print('70%')
-            time.sleep(1)
-            print('90%')
-            time.sleep(1)
-            print('99%')
-            time.sleep(4)
-            print('100%')
-
-
+            dlViaioRedusor(hrefVidio)
 
     else:
         print('Увы, но такой команды нет')
---
+++ finile progect/reduserce/dlViaioRedusor.py
@@ -0,0 +1,79 @@
+# dlViaioRedusor
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+def dlViaioRedusor(hrefVidio):
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.get('https://ru.savefrom.net/1-%D0%B1%D1%8B%D1%81%D1%82%D1%80%D1%8B%D0%B9-%D1%81%D0%BF%D0%BE%D1%81%D0%BE%D0%B1-%D1%81%D0%BA%D0%B0%D1%87%D0%B0%D1%82%D1%8C-%D1%81-youtube-160/')
+
+    # Кнопка, которая ищет по ссылке видио
+    searchVidio = driver.find_element(By.XPATH, '//*[@id="sf_url"]')
+    searchVidio.send_keys(hrefVidio)
+
+    buttonSearchVidio = driver.find_element(By.XPATH, '//*[@id="sf_submit"]')
+    buttonSearchVidio.click()
+    time.sleep(5)
+
+    downloadVidio = driver.find_element(By.XPATH, '//*[@id="sf_result"]/div/div[1]/div[2]/div[2]/div[1]/a')
+    downloadVidio.click()
+
+    time.sleep(1)
+    print('20%')
+    time.sleep(1)
+    print('30%')
+    time.sleep(1)
+    print('40%')
+    time.sleep(1)
+    print('50%')
+    time.sleep(1)
+    print('70%')
+    time.sleep(1)
+    print('90%')
+    time.sleep(1)
+    print('99%')
+    time.sleep(4)
+    print('100%')
+    time.sleep(2)
+    print('Это видио сохранено на ваш компьютер в раздел загрузки')
+
+
+
+# chrome_options = Options()
+# chrome_options.add_argument("--headless")
+
+# def dlViaioRedusor(hrefVidio):
+#     driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+#     driver.get('https://ru.savefrom.net/1-%D0%B1%D1%8B%D1%81%D1%82%D1%80%D1%8B%D0%B9-%D1%81%D0%BF%D0%BE%D1%81%D0%BE%D0%B1-%D1%81%D0%BA%D0%B0%D1%87%D0%B0%D1%82%D1%8C-%D1%81-youtube-160/')
+#
+#     # Кнопка, которая ищет по ссылке видио
+#     searchVidio = driver.find_element(By.XPATH, '//*[@id="sf_url"]')
+#     searchVidio.send_keys(hrefVidio)
+#
+#     buttonSearchVidio = driver.find_element(By.XPATH, '//*[@id="sf_submit"]')
+#     buttonSearchVidio.click()
+#     time.sleep(5)
+#
+#     downloadVidio = driver.find_element(By.XPATH, '//*[@id="sf_result"]/div/div[1]/div[2]/div[2]/div[1]/a')
+#     downloadVidio.click()
+#
+#     time.sleep(1)
+#     print('20%')
+#     time.sleep(1)
+#     print('30%')
+#     time.sleep(1)
+#     print('40%')
+#     time.sleep(1)
+#     print('50%')
+#     time.sleep(1)
+#     print('70%')
+#     time.sleep(1)
+#     print('90%')
+#     time.sleep(1)
+#     print('99%')
+#     time.sleep(4)
+#     print('100%')
+#     time.sleep(2)
+#     print('Это видио сохранено на ваш компьютер в раздел загрузки')
